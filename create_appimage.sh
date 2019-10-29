bb882f25fc047e3effb3a9b028a181f9e412055a
madosuki <yurikasamaaikatsu@gmail.com>
2019-10-29 12:44:31 +0900

    introduce automake

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+SCRIPTDIR=$(dirname ${0})
 
 if type appimagetool-x86_64.AppImage > /dev/null 2>&1; then
-    if [ -e ./build/simple_comics_viewer ]; then
-        ARCH=x86_64 appimagetool-x86_64.AppImage ./build
+    if [ -e ${SCRIPTDIR}/src/simple_comics_viewer ]; then
+
+        echo ${SCRIPTDIR}
+
+        cp ${SCRIPTDIR}/src/simple_comics_viewer ${SCRIPTDIR}/appimage_dir/ &&
+            ARCH=x86_64 appimagetool-x86_64.AppImage ${SCRIPTDIR}/appimage_dir &&
+            rm ${SCRIPTDIR}/appimage_dir/simple_comics_viewer
     fi
+else
+    echo "not found appimagetool-x86_64.AppImage"
 fi
