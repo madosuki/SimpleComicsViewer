49c96fe97c3b5573c437976065eafdf53e199a07
madosuki <yurikasamaaikatsu@gmail.com>
2020-07-07 09:01:58 +0900

    fix option init

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include "viewer.h"
 
+static GOptionEntry option_entries[] = {
+                                        {"file", 'f', 0, G_OPTION_ARG_FILENAME, &arg_file_name, "Open File", "zip or jpg or png"},
+                                        {NULL}
+};
+
+
 int main(int argc, char **argv)
 {
   // app = gtk_application_new("org.gtk.comics_viewer", G_APPLICATION_FLAGS_NONE);
@@ -10,8 +16,7 @@ int main(int argc, char **argv)
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   g_signal_connect(G_APPLICATION(app), "command-line", G_CALLBACK(run_cmd_argument), NULL);
 
-  GOptionEntry entries[] = {{"file", 'f', 0, G_OPTION_ARG_FILENAME, &arg_file_name, "Open File", "zip or jpg or png"}};
-  g_application_add_main_option_entries(G_APPLICATION(app), entries);
+  g_application_add_main_option_entries(G_APPLICATION(app), option_entries);
 
   // declared variable from src/viewer.h
   status = g_application_run(G_APPLICATION(app), argc, argv);
