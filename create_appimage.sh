a316f59739b74f5525ac6eee3acdf1db354dd2ac
madosuki <yurikasamaaikatsu@gmail.com>
2022-11-05 00:44:52 +0900

    change dir name which related appimage

--- create_appimage.sh
+++ create_appimage.sh
@@ -2,15 +2,23 @@
 
 SCRIPTDIR=$(dirname ${0})
 
-if type appimagetool-x86_64.AppImage > /dev/null 2>&1; then
+function create_app_image () {
+    if type appimagetool-x86_64.AppImage > /dev/null 2>&1; then
+        ARCH=x86_64 appimagetool-x86_64.AppImage ${SCRIPTDIR}/appimage_dir
+    else
+        echo "not found appimagetool-x86_64.AppImage"
+    fi
+}
+
+if type linuxdeploy-x86_64.AppImage > /dev/null 2>&1; then
     if [ -e ${SCRIPTDIR}/src/simple_comics_viewer ]; then
-
-        echo ${SCRIPTDIR}
-
-        cp ${SCRIPTDIR}/src/simple_comics_viewer ${SCRIPTDIR}/appimage_dir/ &&
-            ARCH=x86_64 appimagetool-x86_64.AppImage ${SCRIPTDIR}/appimage_dir &&
-            rm ${SCRIPTDIR}/appimage_dir/simple_comics_viewer
+        linuxdeploy-x86_64.AppImage -e ./src/simple_comics_viewer -d ./for_appimage_assets/simple_comics_viewer.desktop -i ./for_appimage_assets/icon.svg --appdir appimage_dir
+        create_app_image
+        rm -rf ./appimage_dir
+    else
+        echo "not done build execute file"
     fi
 else
-    echo "not found appimagetool-x86_64.AppImage"
+    echo "not found linuxdeploy-x86_64.AppImage"
 fi
+
